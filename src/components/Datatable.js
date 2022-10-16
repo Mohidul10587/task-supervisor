338086592d26367c4883d19d84c40361d9802932
mohid <mohid10587@gmail.com>
2022-10-16 16:50:15 +0600

    update design

--- src/components/Datatable.js
+++ src/components/Datatable.js
@@ -23,22 +23,22 @@ const Datatable = () => {
             <div className='flex justify-center mt-4 '>
                 <div className=''>
                     <div className='flex'>
-                        <p className=' w-36 p-2 text-center border border-black font-bold border-l-2 border-t-2'>Folder Name</p>
-                        <p className=' w-36 p-2 text-center border border-black font-bold border-t-2'>Total Image</p>
-                        <p className=' w-36 p-2 text-center border border-black font-bold border-t-2'>Amount</p>
-                        <p className=' w-36 p-2 text-center border border-black font-bold border-t-2'>Google Drive</p>
-                        <p className=' w-36 p-2 text-center border border-black font-bold border-t-2'>Start Date</p>
-                        <p className=' w-36 p-2 text-center border border-black font-bold border-r-2 border-t-2'>Deadline</p>
+                        <p className='bg-gray-700 text-white  w-20 sm:w-36 py-2 px-1 text-center border border-black font-bold border-l-2 border-t-2'>Folder Name</p>
+                        <p className='bg-gray-700 text-white  w-16 sm:w-36 py-2 px-1 text-center border border-black font-bold border-t-2'>Total Image</p>
+                        <p className='bg-gray-700 text-white  w-16 sm:w-36 py-2 px-1 text-center border border-black font-bold border-t-2'>Amount</p>
+                        <p className='bg-gray-700 text-white  w-16 sm:w-36 py-2 px-1 text-center border border-black font-bold border-t-2'>Google Drive</p>
+                        <p className='bg-gray-700 text-white  w-28 sm:w-36 py-2 px-1 text-center border border-black font-bold border-t-2'>Start Date</p>
+                        <p className='bg-gray-700 text-white  w-36 sm:w-36 py-2 px-1 text-center border border-black font-bold border-r-2 border-t-2'>Deadline</p>
 
                     </div>
                     {tasks.map(task => <div key={task._id}>
                         <div className='flex'>
-                            <p className=' w-36 p-2 text-center border border-black border-l-2 border-b-1'>{task.folderName}</p>
-                            <p className=' w-36 p-2 text-center border border-black border-b-1'>{task.totalImage}</p>
-                            <p className=' w-36 p-2 text-center border border-black border-b-1'>{task.amount}</p>
-                            <div className=' w-36 p-2 text-center border border-black border-b-1 flex justify-center'><svg width='15px' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M339 314.9L175.4 32h161.2l163.6 282.9H339zm-137.5 23.6L120.9 480h310.5L512 338.5H201.5zM154.1 67.4L0 338.5 80.6 480 237 208.8 154.1 67.4z" /></svg><span className="pl-4">{task.boolean}</span></div>
-                            <p className=' w-36 p-2 text-center border border-black border-b-1'>{task.startTime}</p>
-                            <div className=' w-36 p-2 text-center border border-black border-r-2 border-b-1'><CounterDown inputTime={task.deadLine} ></CounterDown></div>
+                            <p className=' w-20 sm:w-36 py-2 px-1 text-center border border-black border-l-2 border-b-1'>{task.folderName}</p>
+                            <p className=' w-16 sm:w-36 py-2 px-1 text-center border border-black border-b-1'>{task.totalImage}</p>
+                            <p className=' w-16 sm:w-36 py-2 px-1 text-center border border-black border-b-1'>{task.amount}</p>
+                            <div className=' w-16 sm:w-36 px-4 text-center border border-black border-b-1 flex justify-center items-center'><svg className='min-w-[16px] max-w-[20px]' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M339 314.9L175.4 32h161.2l163.6 282.9H339zm-137.5 23.6L120.9 480h310.5L512 338.5H201.5zM154.1 67.4L0 338.5 80.6 480 237 208.8 154.1 67.4z" /></svg><span className="ml-1 sm:pl-4">{task.boolean}</span></div>
+                            <p className=' w-28 sm:w-36 py-2 px-1 text-center border border-black border-b-1'>{task.startTime}</p>
+                            <div className=' w-36 sm:w-36 py-2 px-1 text-center border border-black border-r-2 border-b-1'><CounterDown inputTime={task.deadLine} ></CounterDown></div>
                         </div>
 
                     </div>)}
--- src/components/Form.js
+++ src/components/Form.js
@@ -24,7 +24,7 @@ const Form = () => {
     const formDataObject = { folderName, totalImage, amount, boolean, startTime, deadLine }
 
 
-    fetch('http://localhost:8000/task', {
+    fetch('https://secure-harbor-61507.herokuapp.com/task', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formDataObject)
@@ -44,26 +44,27 @@ const Form = () => {
   return (
 
     <div className='flex justify-center'>
-      <div className='mt-16'>
+      <div className='mt-12'>
         <h1 className='text-2xl font-bold text-center'>Put Your Job Info</h1>
         <div className='mt-4'>
 
 
           <form onSubmit={formData}>
-            <input className='block border border-black my-1 p-2 rounded-lg w-96' name='folderName' type="text" placeholder='Folder Name' required />
-            <input className='block border border-black my-1 p-2 rounded-lg w-96' name='totalImage' type="number" placeholder='Total Image' required />
-            <input className='block border border-black my-1 p-2 rounded-lg w-96' name='amount' type="number" placeholder='Amount' required />
-            <div className='block border border-black my-1 p-2 rounded-lg w-96'>
+            <input className='block border border-black my-1 p-2 rounded-lg sm:w-96 w-[260px] mx-2' name='folderName' type="text" placeholder='Folder Name' required />
+            <input className='block border border-black my-1 p-2 rounded-lg sm:w-96 w-[260px] mx-2' name='totalImage' type="number" placeholder='Total Image' required />
+            <input className='block border border-black my-1 p-2 rounded-lg sm:w-96 w-[260px] mx-2' name='amount' type="number" placeholder='Amount' required />
+            <div className='block border border-black my-1 p-2 rounded-lg sm:w-96 w-[260px] mx-2'>
               <span className='mr-4'>Google Drive</span>
               <input type="radio" value="Yes" name="boolean" /> Yes
               <input className='ml-3' type="radio" value="No" name="boolean" /> No
             </div>
-            <div className='flex justify-between border border-black my-1 p-2 rounded-lg w-96'>
-              <span>DeadLine</span>
+            <small className='ml-2'>DeadLine</small>
+            <div className='flex justify-between border border-black my-1 p-2 rounded-lg sm:w-96 w-[260px] mx-2'>
+             
               <input name='time' type="date" placeholder='Amount'  required />
               <input name='timeTime' type="time" placeholder='Amount' required />
             </div>
-            <input className='font-bold block border border-black my-1 p-2 rounded-lg w-96 hover:bg-gray-600 hover:text-white' type="submit" value="Submit" />
+            <input className='font-bold block border border-black my-1 p-2 rounded-lg sm:w-96 w-[260px] mx-2 hover:bg-gray-600 hover:text-white' type="submit" value="Submit" />
 
           </form>
 
